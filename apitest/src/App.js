6ed13503d71fac05530555cfe9724de8c69d423e
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-04-04 02:17:53 +0900

    Make View

--- apitest/src/App.js
+++ apitest/src/App.js
@@ -1,38 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 import axios from 'axios';
 
+import React, { useState } from 'react';
+
 function App() {
+  const [posts, setPosts] = useState([]);
   // GET通信
-  axios.get('http://localhost/html/Blog/api2.php?grade=1')
+  axios.get('http://utan.php.xdomain.jp/lovelivedatabase/api.php?groups=all')
 
   // thenで成功した場合の処理をかける
   .then(response => {
-      console.log('status:', response.status); // 200
-      console.log('body:', response.data);     // response body.
+      // console.log('status:', response.status); // 200
+      // console.log('body:', response.data);     // response body.
 
+      setPosts(response.data);
   // catchでエラー時の挙動を定義する
   }).catch(err => {
       console.log('err:', err);
   });
+
+  console.log(posts);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {posts.map((post) => (
+      <>
+        <p>{post.name}</p>
+        <p>CV:{post.CV}</p>
+        <p>{post.grade}年生</p>
+        <p>{post.birthday}生まれ</p>
+        <p>{post.bloodType}型</p>
+        <p>{post.height}cm</p>
+        <img style={{width:'200px'}} src={post.image} alt='画像'/>
+      </>
+      ))}
+    </>
   );
 }
 
